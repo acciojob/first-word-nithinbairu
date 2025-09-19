1051198d4fb646babb4b3bf3e47ff7f788e3c721
nithinbairu <113778598+nithinbairu@users.noreply.github.com>
2025-09-19 16:26:59 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,15 @@
 function firstWord(s) {
   // your code here
+	let ans=''
+	let i=0;
+	while (i<s.length&&s[i]===' ') {
+		i++
+	}
+	while (i<s.length&&s[i]!==' ') {
+		ans+=s[i];
+		i++;
+	}
+	return ans
 }
 
 // Do not change the code below
